57e7bc01d4997004fcab90a95ab9c53a1236f788
Cebrián <javiceb@hotmail.com>
2023-04-16 23:21:14 +0200

    Add files via upload

---
+++ src/main/java/concatena/typical/TypicalApplication.java
@@ -0,0 +1,13 @@
+package concatena.typical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/concatena/typical/TypicalApplicationTests.java
@@ -0,0 +1,13 @@
+package concatena.typical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
